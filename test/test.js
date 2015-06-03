43e1b2dd93234b401e9345e86dfc522077d6833d
Tyler Johnson <tyler@vintyge.com>
2015-06-03 17:11:40 -0600

    added some tests for the changes feed

--- test/test.js
+++ test/test.js
@@ -23,7 +23,7 @@ describe("CRUD", function() {
 		pouch.destroy(done);
 	});
 
-	it("creates a document", function(done) {
+	it("creates a model", function(done) {
 		var model = col.add({ foo: "bar" });
 
 		model.save().then(function(res) {
@@ -39,7 +39,7 @@ describe("CRUD", function() {
 		.catch(done);
 	});
 
-	it("deletes a document", function(done) {
+	it("deletes a model", function(done) {
 		var model = col.add({ foo: "bar" });
 
 		model.save().then(function() {
@@ -60,7 +60,7 @@ describe("CRUD", function() {
 		.catch(done);
 	});
 
-	it("updates a document", function(done) {
+	it("updates a model", function(done) {
 		var model = col.add({ foo: "bar" });
 
 		model.save().then(function() {
@@ -80,7 +80,7 @@ describe("CRUD", function() {
 		.catch(done);
 	});
 
-	it("reads a database", function(done) {
+	it("reads a collection", function(done) {
 		pouch.bulkDocs([
 			{ _id: "a" },
 			{ _id: "b" },
@@ -102,7 +102,7 @@ describe("CRUD", function() {
 		.catch(done);
 	});
 
-	it("reads a document", function(done) {
+	it("reads a model", function(done) {
 		var model = col.add({ _id: "testmodel" });
 
 		return pouch.put({
@@ -124,10 +124,117 @@ describe("CRUD", function() {
 
 });
 
-describe("Live Sync", function() {
+describe("Changes Feed", function() {
+	var col, pouch;
 
-});
+	this.slow(1000);
+	this.timeout(5000);
 
-describe("Conflicts", function() {
+	beforeEach(function() {
+		pouch = new PouchDB("testdb");
+		var sync = pouch.lazybones();
+		col = new Backbone.Collection(null);
+		col.model = Backbone.Model.extend({ sync: sync, idAttribute: "_id" });
+		col.sync = sync;
+	});
+
+	afterEach(function(done) {
+		pouch.destroy(done);
+	});
+
+	it("adds model from changes", function(done) {
+		pouch.post({ foo: "bar" })
+
+		.then(function(res) {
+			expect(res.ok).to.be.ok;
+			return col.fetch({ changes: true });
+		})
+
+		.then(function() {
+			expect(col.length).to.equal(1);
+			var model = col.first();
+			expect(model.isNew()).to.not.be.ok;
+			expect(model.get("foo")).to.equal("bar");
+			done();
+		})
+
+		.catch(done);
+	});
+
+	it("updates model from changes", function(done) {
+		// add doc
+		pouch.post({ foo: "bar" })
+
+		// base collection fetch
+		.then(function(res) {
+			return col.fetch()
+
+			// update the model
+			.then(function() {
+				expect(col.length).to.equal(1);
+				return pouch.put({ _id: res.id, _rev: res.rev, foo: "baz" });
+			});
+		})
+
+		// fetch from changes feed
+		.then(function(res) {
+			expect(res.ok).to.be.ok;
+			return col.fetch({ changes: true });
+		})
+
+		.then(function() {
+			expect(col.length).to.equal(1);
+			var model = col.first();
+			expect(model.isNew()).to.not.be.ok;
+			expect(model.get("foo")).to.equal("baz");
+			done();
+		})
+
+		.catch(done);
+	});
+
+	it("removes model from changes", function(done) {
+		// add doc
+		pouch.post({ foo: "bar" })
+
+		// base collection fetch
+		.then(function(res) {
+			return col.fetch()
+
+			// remove the model
+			.then(function() {
+				expect(col.length).to.equal(1);
+				return pouch.remove(res.id, res.rev);
+			});
+		})
+
+		// fetch again from changes feed
+		.then(function(res) {
+			expect(res.ok).to.be.ok;
+			return col.fetch({ changes: true });
+		})
+
+		.then(function() {
+			expect(col.length).to.equal(0);
+			done();
+		})
+
+		.catch(done);
+	});
+
+	it("listens for live changes", function(done) {
+		col.fetch({ changes: { live: true } });
+		expect(col.length).to.equal(0);
+
+		col.on("add", function(model) {
+			expect(col.length).to.equal(1);
+			expect(model.isNew()).to.not.be.ok;
+			expect(model.get("foo")).to.equal("bar");
+			done();
+		});
+
+		// add doc
+		pouch.post({ foo: "bar" }).catch(done);
+	});
 
 });
